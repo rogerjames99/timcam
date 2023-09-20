7ded2a97f963b38f7cfcff819a66e5fb60cb541f
Roger James <roger@beardandsandals.co.uk>
2023-09-20 14:12:58 +0100

    Switched to default espressif sketch.

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,51 @@
-#include <Arduino.h>
-#include <WiFi.h>
-#include <WiFiClientSecure.h>
-#include "soc/soc.h"
-#include "soc/rtc_cntl_reg.h"
 #include "esp_camera.h"
-#include <UniversalTelegramBot.h>
-#include <ArduinoJson.h>
-#include "DFRobot_AXP313A.h"
+#include <WiFi.h>
 
-DFRobot_AXP313A axp;
+//
+// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
+//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
+//            Partial images will be transmitted if image exceeds buffer size
+//
+//            You must select partition scheme from the board menu that has at least 3MB APP space.
+//            Face Recognition is DISABLED for ESP32 and ESP32-S2, because it takes up from 15 
+//            seconds to process single frame. Face Detection is ENABLED if PSRAM is enabled as well
 
-const char* ssid = "ELDRADO";
-const char* password = "amazon123";
+// ===================
+// Select camera model
+// ===================
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+//#define CAMERA_MODEL_ESP32S3_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
+//#define CAMERA_MODEL_M5STACK_UNITCAM // No PSRAM
+//#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+//#define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
+// ** Espressif Internal Boards **
+//#define CAMERA_MODEL_ESP32_CAM_BOARD
+//#define CAMERA_MODEL_ESP32S2_CAM_BOARD
+//#define CAMERA_MODEL_ESP32S3_CAM_LCD
+#define CAMERA_MODEL_DFRobot_FireBeetle2_ESP32S3 // Has PSRAM
+//#define CAMERA_MODEL_DFRobot_Romeo_ESP32S3 // Has PSRAM
+#include "camera_pins.h"
 
-// Initialize Telegram BOT
-String BOTtoken = "5846867606:AAGS9FTjwwGbcgrJRe8K0G83Ibnp0nEvFu0";
-String CHAT_ID = "5379039379";
+// ===========================
+// Enter your WiFi credentials
+// ===========================
+const char* ssid = "timcam";
+const char* password = "woodies";
 
-bool sendPhoto = false;
+void startCameraServer();
+void setupLedFlash(int pin);
 
-WiFiClientSecure clientTCP;
-UniversalTelegramBot bot(BOTtoken, clientTCP);
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
 
-#define FLASH_LED_PIN 21
-bool flashState = LOW;
-
-int botRequestDelay = 1000;
-unsigned long lastTimeBotRan;
-
-#define PWDN_GPIO_NUM     -1
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM     45
-#define SIOD_GPIO_NUM     1
-#define SIOC_GPIO_NUM     2
-
-#define Y9_GPIO_NUM       48
-#define Y8_GPIO_NUM       46
-#define Y7_GPIO_NUM       8
-
-#define Y6_GPIO_NUM       7
-#define Y5_GPIO_NUM       4
-#define Y4_GPIO_NUM       41
-#define Y3_GPIO_NUM       40
-#define Y2_GPIO_NUM       39
-#define VSYNC_GPIO_NUM    6
-#define HREF_GPIO_NUM     42
-#define PCLK_GPIO_NUM     5
-
-void configInitCamera() {
-  axp.enableCameraPower(axp.eOV2640);//设置摄像头供电
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -69,195 +66,90 @@ void configInitCamera() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-
-  //init with high specs to pre-allocate larger buffers
-  if (psramFound()) {
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;  //0-63 lower number means higher quality
-    config.fb_count = 2;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+  
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if(config.pixel_format == PIXFORMAT_JPEG){
+    if(psramFound()){
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      // Limit the frame size when PSRAM is not available
+      config.frame_size = FRAMESIZE_SVGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
   } else {
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12;  //0-63 lower number means higher quality
-    config.fb_count = 1;
+    // Best option for face detection/recognition
+    config.frame_size = FRAMESIZE_240X240;
+#if CONFIG_IDF_TARGET_ESP32S3
+    config.fb_count = 2;
+#endif
   }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
 
   // camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
-    delay(1000);
-    ESP.restart();
+    return;
   }
 
-  // Drop down frame size for higher initial frame rate
   sensor_t * s = esp_camera_sensor_get();
-  s->set_framesize(s, FRAMESIZE_CIF);
-}
-
-void handleNewMessages(int numNewMessages) {
-  Serial.print("Handle New Messages: ");
-  Serial.println(numNewMessages);
-
-  for (int i = 0; i < numNewMessages; i++) {
-    String chat_id = String(bot.messages[i].chat_id);
-    if (chat_id != CHAT_ID) {
-      bot.sendMessage(chat_id, "Unauthorized user", "");
-      continue;
-    }
-
-    // Print the received message
-    String text = bot.messages[i].text;
-    Serial.println(text);
-
-    String from_name = bot.messages[i].from_name;
-    if (text == "/start") {
-      String welcome = "Welcome , " + from_name + "n";
-      welcome += "Use the following commands to interact with the ESP32-CAM n";
-      welcome += "/photo : takes a new photon";
-      welcome += "/flash : toggles flash LED n";
-      bot.sendMessage(CHAT_ID, welcome, "");
-    }
-    if (text == "/flash") {
-      flashState = !flashState;
-      digitalWrite(FLASH_LED_PIN, flashState);
-      Serial.println("Change flash LED state");
-    }
-    if (text == "/photo") {
-      sendPhoto = true;
-      Serial.println("New photo request");
-    }
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1); // flip it back
+    s->set_brightness(s, 1); // up the brightness just a bit
+    s->set_saturation(s, -2); // lower the saturation
   }
-}
-
-String sendPhotoTelegram() {
-  const char* myDomain = "api.telegram.org";
-  String getAll = "";
-  String getBody = "";
-
-  camera_fb_t * fb = NULL;
-  fb = esp_camera_fb_get();
-  if (!fb) {
-    Serial.println("Camera capture failed");
-    delay(1000);
-    ESP.restart();
-    return "Camera capture failed";
+  // drop down frame size for higher initial frame rate
+  if(config.pixel_format == PIXFORMAT_JPEG){
+    s->set_framesize(s, FRAMESIZE_QVGA);
   }
 
-  Serial.println("Connect to " + String(myDomain));
+#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
 
+#if defined(CAMERA_MODEL_ESP32S3_EYE)
+  s->set_vflip(s, 1);
+#endif
 
-  if (clientTCP.connect(myDomain, 443)) {
-    Serial.println("Connection successful");
+// Setup LED FLash if LED pin is defined in camera_pins.h
+#if defined(LED_GPIO_NUM)
+  setupLedFlash(LED_GPIO_NUM);
+#endif
 
-String head = "--DFRobotBot\nContent-Disposition: form-data; name=\"chat_id\"; \n\n" + CHAT_ID + "\n--DFRobotBot\nContent-Disposition: form-data\; name=\"photo\"; filename=\"esp32-cam.jpg\"\nContent-Type: image/jpeg\n";    String tail = "rn--DFRobotBot--rn";
-
-    uint16_t imageLen = fb->len;
-    uint16_t extraLen = head.length() + tail.length();
-    uint16_t totalLen = imageLen + extraLen;
-
-    clientTCP.println("POST /bot" + BOTtoken + "/sendPhoto HTTP/1.1");
-    clientTCP.println("Host: " + String(myDomain));
-    clientTCP.println("Content-Length: " + String(totalLen));
-    clientTCP.println("Content-Type: multipart/form-data; boundary=DFRobotBot");
-    clientTCP.println();
-    clientTCP.print(head);
-
-    uint8_t *fbBuf = fb->buf;
-    size_t fbLen = fb->len;
-    for (size_t n = 0; n < fbLen; n = n + 1024) {
-      if (n + 1024 < fbLen) {
-        clientTCP.write(fbBuf, 1024);
-        fbBuf += 1024;
-      }
-      else if (fbLen % 1024 > 0) {
-        size_t remainder = fbLen % 1024;
-        clientTCP.write(fbBuf, remainder);
-      }
-    }
-
-    clientTCP.print(tail);
-
-    esp_camera_fb_return(fb);
-
-    int waitTime = 10000;   // timeout 10 seconds
-    long startTimer = millis();
-    boolean state = false;
-
-    while ((startTimer + waitTime) > millis()) {
-      Serial.print(".");
-      delay(100);
-      while (clientTCP.available()) {
-        char c = clientTCP.read();
-        if (state == true) getBody += String(c);
-        if (c == 'n') {
-          if (getAll.length() == 0) state = true;
-          getAll = "";
-        }
-        else if (c != 'r')
-          getAll += String(c);
-        startTimer = millis();
-      }
-      if (getBody.length() > 0) break;
-    }
-    clientTCP.stop();
-    Serial.println(getBody);
-  }
-  else {
-    getBody = "Connected to api.telegram.org failed.";
-    Serial.println("Connected to api.telegram.org failed.");
-  }
-  return getBody;
-}
-
-void setup() {
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
-  // Init Serial Monitor
-  Serial.begin(115200);
-
-  // Set LED Flash as output
-  pinMode(FLASH_LED_PIN, OUTPUT);
-  digitalWrite(FLASH_LED_PIN, flashState);
-
-  while (axp.begin() != 0) {
-    Serial.println("init error");
-    delay(1000);
-  }
-
-  // Config and init the camera
-  configInitCamera();
-
-  // Connect to Wi-Fi
-  WiFi.mode(WIFI_STA);
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
   WiFi.begin(ssid, password);
-  clientTCP.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  WiFi.setSleep(false);
+
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
     delay(500);
+    Serial.print(".");
   }
-  Serial.println();
-  Serial.print("FireBeetle 2 ESP32 S3-CAM IP Address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("");
+  Serial.println("WiFi connected");
 
+  startCameraServer();
 
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
 }
 
 void loop() {
-  if (sendPhoto) {
-    Serial.println("Preparing photo");
-    sendPhotoTelegram();
-    sendPhoto = false;
-  }
-  if (millis() > lastTimeBotRan + botRequestDelay)  {
-    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-    while (numNewMessages) {
-      Serial.println("got response");
-      handleNewMessages(numNewMessages);
-      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-    }
-    lastTimeBotRan = millis();
-  }
+  // Do nothing. Everything is done in another task by the web server
+  delay(10000);
 }
